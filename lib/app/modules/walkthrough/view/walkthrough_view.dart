e9e673c2f15da424ed778d9b26c274c1bc6c983f
Mhyth <leanderjose7@gmail.com>
2023-03-14 21:50:13 +0700

    Carousel

--- lib/app/modules/walkthrough/view/walkthrough_view.dart
+++ lib/app/modules/walkthrough/view/walkthrough_view.dart
@@ -1,9 +1,9 @@
-import '../../../routes/app_pages.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttericon/entypo_icons.dart';
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../../../utils/app_constants.dart';
 import '../controllers/walkthrough_controller.dart';
 
@@ -18,45 +18,64 @@ class WalkthroughView extends GetView<WalkthroughController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          children: [
-            CarouselSlider(
-              items: controller.imageWithTextList,
-              options: CarouselOptions(
-                autoPlay: true,
-                height: Get.height * 0.4 * GOLDEN_RATIO,
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+              horizontal: CONTAINER_MARGIN_HORIZONTAL,
+              vertical: CONTAINER_MARGIN_VERTICAL),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: CarouselSlider(
+                  items: controller.imageWithTextList,
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    aspectRatio: 2 / 3,
+                    viewportFraction: 1,
+                    enableInfiniteScroll: false,
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 10 * GOLDEN_RATIO,
-            ),
-            FilledButton.icon(
-              onPressed: () {
-                Get.toNamed(Routes.REGISTER);
-              },
-              icon: const Icon(Entypo.mail),
-              label: const Text('Sign Up With Email'),
-            ),
-            const SizedBox(
-              height: 10 * GOLDEN_RATIO,
-            ),
-            OutlinedButton(
-              onPressed: () {
-                Get.offNamed(Routes.LOGIN);
-              },
-              child: const Text.rich(
-                TextSpan(
-                  text: "Already Have Account? ",
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: "Sign In",
-                      style: TextStyle(decoration: TextDecoration.underline),
+              const SizedBox(
+                height: 5 * GOLDEN_RATIO,
+              ),
+              SizedBox(
+                height: Get.height * 0.1 * GOLDEN_RATIO,
+                child: Column(
+                  children: [
+                    FilledButton.icon(
+                      onPressed: () {
+                        Get.toNamed(Routes.REGISTER);
+                      },
+                      icon: const Icon(Entypo.mail),
+                      label: const Text('Sign Up With Email'),
+                    ),
+                    const SizedBox(
+                      height: 10 * GOLDEN_RATIO,
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+                        Get.offNamed(Routes.LOGIN);
+                      },
+                      child: const Text.rich(
+                        TextSpan(
+                          text: "Already Have Account? ",
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: "Sign In",
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
