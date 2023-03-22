d3b85f0e616d56ec36415904248b96d538fe8eac
Mhyth <leanderjose7@gmail.com>
2023-03-22 18:42:41 +0700

    Fix query for friend tab
    Fix query for friend tab, refactor controller, add friend provider, add query for manage friend

--- lib/app/widgets/friendBottomSheet/views/friend_bottom_sheet_view.dart
+++ lib/app/widgets/friendBottomSheet/views/friend_bottom_sheet_view.dart
@@ -1,25 +1,57 @@
-import 'package:bill_splitter/app/utils/app_constants.dart';
-import 'package:bill_splitter/app/widgets/friendBottomSheet/controllers/friend_bottom_sheet_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class AddFriendBottomSheet extends GetView<FriendBottomSheetController> {
-  const AddFriendBottomSheet({super.key});
+import '../../../utils/app_constants.dart';
+import '../../../utils/validations_helper.dart';
+import '../controllers/friend_bottom_sheet_controller.dart';
+
+class FriendBottomSheetView extends StatelessWidget {
+  FriendBottomSheetView({super.key});
+  final controller = Get.put(FriendBottomSheetController());
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(
+          horizontal: 15 * GOLDEN_RATIO, vertical: CONTAINER_MARGIN_VERTICAL),
       color: Colors.white,
-      height: 200 * GOLDEN_RATIO,
       width: Get.width,
-      child: Row(children: [
-        Text(
-          'Add New Friends',
-          style:
-              Get.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Add New Friends',
+              style: Get.textTheme.titleLarge
+                  ?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10 * GOLDEN_RATIO,
+            ),
+            Text(
+              'Email Address',
+              style: Get.textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 2 * GOLDEN_RATIO,
+            ),
+            TextFormField(
+              controller: controller.friendEmailController,
+              decoration:
+                  const InputDecoration(hintText: 'Enter Email Address'),
+              validator: emailValidator,
+            ),
+            const SizedBox(
+              height: 30 * GOLDEN_RATIO,
+            ),
+            FilledButton(
+                onPressed: () {
+                  controller.addFriend();
+                },
+                child: const Text('Send Friend Request'))
+          ],
         ),
-        TextFormField()
-      ]),
+      ),
     );
   }
 }
