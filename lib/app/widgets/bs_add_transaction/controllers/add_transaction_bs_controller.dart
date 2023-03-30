4367a0df3b490635bc2561f5910fd6e9e93ff23b
Dikky Larson <dikky156@gmail.com>
2023-03-30 19:09:10 +0700

    add validation add trx

--- lib/app/widgets/bs_add_transaction/controllers/add_transaction_bs_controller.dart
+++ lib/app/widgets/bs_add_transaction/controllers/add_transaction_bs_controller.dart
@@ -7,14 +7,18 @@ import '../../../utils/validations_helper.dart';
 class AddTransactionBsController extends GetxController {
   final nameController = TextEditingController();
   final selectDateController = TextEditingController();
-  DateTime? selectedDate = DateTime.now();
+  final formKey = GlobalKey<FormState>();
 
+  DateTime? selectedDate = DateTime.now();
   RxBool isLoading = false.obs;
-  void setDate(List<DateTime?> dates) {
-    selectedDate = dates.first;
+
+  void setDate(DateTime? value) {
+    selectedDate = value;
   }
 
   Future addTransaction() async {
+    if (!formKey.currentState!.validate()) return;
+    isLoading.value = true;
     try {
       await supabaseClient.from('TransactionHeader').insert({
         'Name': nameController.text,
@@ -23,6 +27,8 @@ class AddTransactionBsController extends GetxController {
     } catch (e) {
       if (Get.isSnackbarOpen) await Get.closeCurrentSnackbar();
       Get.snackbar(unexpectedErrorText, e.toString());
+    } finally {
+      isLoading.value = false;
     }
   }
 }
--- lib/app/widgets/bs_add_transaction/views/add_transaction_bs_view.dart
+++ lib/app/widgets/bs_add_transaction/views/add_transaction_bs_view.dart
@@ -1,6 +1,8 @@
+import 'package:bill_splitter/app/utils/validations_helper.dart';
 import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttericon/font_awesome_icons.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:nb_utils/nb_utils.dart';
@@ -31,82 +33,97 @@ class AddTransactionBottomSheet extends StatelessWidget {
             horizontal: 10 * GOLDEN_RATIO,
             vertical: 20 * GOLDEN_RATIO,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Add Transaction',
-                style: Get.textTheme.labelMedium?.copyWith(
-                  fontSize: 12 * GOLDEN_RATIO,
-                  fontWeight: FontWeight.w900,
+          child: Form(
+            key: controller.formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Add Transaction',
+                  style: Get.textTheme.labelMedium?.copyWith(
+                    fontSize: 12 * GOLDEN_RATIO,
+                    fontWeight: FontWeight.w900,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 12 * GOLDEN_RATIO,
-              ),
-              Text(
-                'Transaction Name',
-                style: Get.textTheme.labelMedium,
-              ),
-              const SizedBox(
-                height: 3 * GOLDEN_RATIO,
-              ),
-              TextFormField(
-                controller: controller.nameController,
-                decoration: const InputDecoration(
-                  hintText: 'Name',
+                const SizedBox(
+                  height: 12 * GOLDEN_RATIO,
                 ),
-              ),
-              const SizedBox(
-                height: 12 * GOLDEN_RATIO,
-              ),
-              Text(
-                'Transaction Date',
-                style: Get.textTheme.labelMedium,
-              ),
-              const SizedBox(
-                height: 3 * GOLDEN_RATIO,
-              ),
-              TextFormField(
-                controller: controller.selectDateController,
-                readOnly: true,
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(FontAwesome.calendar),
-                    hintText: 'Tap to select date'),
-                onTap: () async {
-                  final selectedDate = await showCalendarDatePicker2Dialog(
-                    context: context,
-                    config: CalendarDatePicker2WithActionButtonsConfig(
-                      firstDayOfWeek: 1,
-                      selectedDayHighlightColor: getColorFromHex(COLOR_1),
-                      controlsTextStyle: Get.textTheme.labelMedium,
-                    ),
-                    dialogSize: Size(
-                      Get.width * 0.7 * GOLDEN_RATIO,
-                      400,
-                    ),
-                    value: [controller.selectedDate],
-                  );
+                Text(
+                  'Transaction Name',
+                  style: Get.textTheme.labelMedium,
+                ),
+                const SizedBox(
+                  height: 3 * GOLDEN_RATIO,
+                ),
+                TextFormField(
+                  controller: controller.nameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Name',
+                  ),
+                  validator: RequiredValidator(errorText: requiredErrorText),
+                ),
+                const SizedBox(
+                  height: 12 * GOLDEN_RATIO,
+                ),
+                Text(
+                  'Transaction Date',
+                  style: Get.textTheme.labelMedium,
+                ),
+                const SizedBox(
+                  height: 3 * GOLDEN_RATIO,
+                ),
+                TextFormField(
+                  controller: controller.selectDateController,
+                  validator: RequiredValidator(errorText: requiredErrorText),
+                  readOnly: true,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(FontAwesome.calendar),
+                      hintText: 'Tap to select date'),
+                  onTap: () async {
+                    final selectedDate = await showCalendarDatePicker2Dialog(
+                      context: context,
+                      config: CalendarDatePicker2WithActionButtonsConfig(
+                        firstDayOfWeek: 1,
+                        selectedDayHighlightColor: getColorFromHex(COLOR_1),
+                        controlsTextStyle: Get.textTheme.labelMedium,
+                      ),
+                      dialogSize: Size(
+                        Get.width * 0.7 * GOLDEN_RATIO,
+                        400,
+                      ),
+                      value: [controller.selectedDate],
+                    );
 
-                  controller.selectDateController.text = selectedDate != null
-                      ? (selectedDate[0] != null
-                          ? DateFormat('dd MMM yyyy').format(selectedDate[0]!)
-                          : controller.selectDateController.text)
-                      : controller.selectDateController.text;
-                },
-              ),
-              const SizedBox(
-                height: 25 * GOLDEN_RATIO,
-              ),
-              FilledButton.icon(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesome.plus,
-                  size: 10 * GOLDEN_RATIO,
+                    controller.selectDateController.text = selectedDate != null
+                        ? (selectedDate[0] != null
+                            ? DateFormat('dd MMM yyyy').format(selectedDate[0]!)
+                            : controller.selectDateController.text)
+                        : controller.selectDateController.text;
+
+                    if (selectedDate != null && selectedDate[0] != null) {
+                      controller.setDate(selectedDate[0]);
+                    }
+                  },
                 ),
-                label: const Text('Add New Transaction'),
-              )
-            ],
+                const SizedBox(
+                  height: 25 * GOLDEN_RATIO,
+                ),
+                FilledButton.icon(
+                  onPressed: controller.isLoading.isFalse
+                      ? () async {
+                          await controller.addTransaction();
+                        }
+                      : null,
+                  icon: const Icon(
+                    FontAwesome.plus,
+                    size: 10 * GOLDEN_RATIO,
+                  ),
+                  label: controller.isLoading.isFalse
+                      ? const Text('Add New Transaction')
+                      : const Text('Saving...'),
+                )
+              ],
+            ),
           ),
         ),
       ],
