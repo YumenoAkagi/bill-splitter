9661ab788bf424d1dcbbc90052e319233acc3878
Dikky Larson <dikky156@gmail.com>
2023-04-10 17:10:36 +0700

    update split equal formula

--- lib/app/modules/transactions/split_transactions_options/controllers/split_trx_options_controller.dart
+++ lib/app/modules/transactions/split_transactions_options/controllers/split_trx_options_controller.dart
@@ -16,30 +16,56 @@ class SplitTrxOptionsController extends GetxController {
     try {
       final whoPaidController = Get.find<WhoPaidTrxBillController>();
       final membersWhoPaidBill = whoPaidController.membersWhoPaidBill;
-      for (var i = 0; i < membersWhoPaidBill.length; i++) {
-        for (var j = 0; j < trxHeader.membersList.length; j++) {
-          // add to db
-          final amountOwed =
-              membersWhoPaidBill[i].paidAmount / trxHeader.membersList.length;
-          final isInserted = await _transactionRepo.addTransactionUser(
-            trxHeader.id,
-            trxHeader.membersList[j].id,
-            membersWhoPaidBill[i].member.id,
-            amountOwed.ceilToDouble(),
-          );
+      final totalEquallySplitted =
+          trxHeader.grandTotal / trxHeader.membersList.length;
+      final totalAmountPaidByMembers = whoPaidController.membersWhoPaidBill
+          .fold(0.0, (prev, trxPaid) => prev + trxPaid.paidAmount);
 
-          if (!isInserted) {
-            // revert all inserted trx
-            await _transactionRepo.deleteAllTransactionUser(trxHeader.id);
-            // showErrorSnackbar(
-            //     "Error occurred when trying to split the bill. Please try again");
-            return;
+      bool isSucceed = true;
+
+      for (var i = 0; i < trxHeader.membersList.length; i++) {
+        final currentMember = trxHeader.membersList[i];
+        for (var j = 0; i < membersWhoPaidBill.length; j++) {
+          if (membersWhoPaidBill.firstWhereOrNull(
+                  (trxPaid) => trxPaid.member.id == currentMember.id) ==
+              null) {
+            // if the member not paying the bill, then the member gets all the total amount owed splitted
+            isSucceed = await _transactionRepo.addTransactionUser(
+              trxHeader.id,
+              currentMember.id,
+              membersWhoPaidBill[j].member.id,
+              membersWhoPaidBill[j].paidAmount /
+                  totalAmountPaidByMembers *
+                  totalEquallySplitted,
+            );
+          } else {
+            // if the member is also paying for the bill, check if the member has debts
+            final debtsOrSurplusAmount =
+                membersWhoPaidBill[j].paidAmount - totalAmountPaidByMembers;
+            if (debtsOrSurplusAmount < 0.0 &&
+                membersWhoPaidBill[j].member.id != currentMember.id) {
+              // if have debts, share equally to other members excl. this member
+              isSucceed = await _transactionRepo.addTransactionUser(
+                trxHeader.id,
+                currentMember.id,
+                membersWhoPaidBill[j].member.id,
+                (membersWhoPaidBill[j].paidAmount /
+                        totalAmountPaidByMembers *
+                        debtsOrSurplusAmount)
+                    .abs(),
+              );
+            }
           }
 
-          isLoading.value = false;
-          Get.offNamed(Routes.SPLITSUCCESS);
+          if (!isSucceed) {
+            // revert
+            await _transactionRepo.deleteAllTransactionUser(trxHeader.id);
+            return;
+          }
         }
       }
+      isLoading.value = false;
+      Get.offNamed(Routes.SPLITSUCCESS);
     } catch (e) {
       showUnexpectedErrorSnackbar(e);
     }
